4332f5dce66eb14f7dfd7a747c6c6fe71a2a859e
Rahul Kinra <38756353+rahulkinra@users.noreply.github.com>
2018-07-28 11:55:14 +0530

    Add files via upload

---
+++ factorial of a number.py
@@ -0,0 +1,17 @@
+# Factorial of a number
+#print('Enter a Number');
+#n1 = input();
+#num1 = int(n1);
+f=1;
+x=1;
+while x<=3:
+    f=f*x;
+    x+=1;
+
+print(f);
+
+#for(int i=1 ; i<num1 ; i++):
+ #   f = f*i;
+
+#print(f);
+
