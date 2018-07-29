500d007a358798c18207af99f2b74357e5611808
Rahul Kinra <38756353+rahulkinra@users.noreply.github.com>
2018-07-29 14:42:09 +0530

    Add files via upload

---
+++ prime number.py
@@ -0,0 +1,16 @@
+# find the prime number between 1 to 30 in python
+
+
+
+lower = 1
+upper = 30
+print("Prime numbers between",lower,"and",upper,"are:")
+
+for num in range(lower,upper + 1):
+   # prime numbers are greater than 1
+   if num > 1:
+       for i in range(2,num):
+           if (num % i) == 0:
+               break
+       else:
+           print(num)
